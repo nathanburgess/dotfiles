33354a4ae5fd40e356cde2f338721624f655dd75
Nathan Burgess <nathanburgess@users.noreply.github.com>
2017-01-10 17:52:45 -0500

    Reordered commands

--- install.sh
+++ install.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-# Set the user's default shell to ZSH
-chsh -s /bin/zsh
+# Update aptitude and install zsh
+echo -e "\n\r \e[36m--\e[32m Updating Aptitude and installing ZSH... \e[36m--\e[0m"
+sudo apt-get update &> /dev/null
+sudo apt-get install zsh -y &> /dev/null
 
 # Create a temporary script to execute with ZSH instead of BASH
 cat > z << EOF
 #!/bin/zsh
 
-# Update aptitude and install zsh
-
-echo "\n\r \e[36m--\e[32m Updating Aptitude and installing ZSH... \e[36m--\e[0m"
-sudo apt-get update &> /dev/null
-sudo apt-get install zsh -y &> /dev/null
-
-echo "\n\r \e[36m--\e[32m Cloning Prezto to HOME directory... \e[36m--\e[0m"
+# Set the user's default shell to ZSH
+chsh -s /bin/zsh
 
 # Clone Prezto
+echo "\n\r \e[36m--\e[32m Cloning Prezto to HOME directory... \e[36m--\e[0m"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "\$HOME/.zprezto" &> /dev/null
 
+# Link the Prezto config files
 echo "\n\r \e[36m--\e[32m Linking Prezto configuration files \e[36m--\e[0m"
-
 setopt EXTENDED_GLOB
 for rcfile in "\$HOME"/.zprezto/runcoms/^README.md(.N); do
   ln -s "\$rcfile" "\$HOME/.\${rcfile:t}" &> /dev/null
 done
 
+# Copy my dotfiles to the HOME directory
 echo "\n\r \e[36m--\e[32m Copying dotfiles to HOME directory \e[36m--\e[0m"
 find configs -type f -print0 | xargs -0 -I file cp -v -p file --target-directory=\$HOME
 
@@ -34,6 +33,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.
 # Install some Vim plugins
 vim -c ":PlugInstall | q | q" 
 
+# Clear color
 echo "\e[0m"
 EOF
 
