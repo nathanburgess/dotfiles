4e513510991bd0eb9ac4ecc7c3fa71d64b69d756
Nathan Burgess <nathanburgess@users.noreply.github.com>
2017-01-10 18:00:33 -0500

    Added success message

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ echo -e "\n\r \e[36m--\e[32m Updating Aptitude and installing ZSH... \e[36m--\e[
 sudo apt-get update &> /dev/null
 sudo apt-get install zsh -y &> /dev/null
 
-# Create a temporary script to execute with ZSH instead of BASH
-cat > z << EOF
-#!/bin/zsh
-
 # Set the user's default shell to ZSH
 chsh -s /bin/zsh
 
 # Clone Prezto
 echo "\n\r \e[36m--\e[32m Cloning Prezto to HOME directory... \e[36m--\e[0m"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "\$HOME/.zprezto" &> /dev/null
+
+# Create a temporary script to execute with ZSH instead of BASH
+cat > z << EOF
+#!/bin/zsh
 
 # Link the Prezto config files
 echo "\n\r \e[36m--\e[32m Linking Prezto configuration files \e[36m--\e[0m"
@@ -26,16 +26,15 @@ done
 # Copy my dotfiles to the HOME directory
 echo "\n\r \e[36m--\e[32m Copying dotfiles to HOME directory \e[36m--\e[0m"
 find configs -type f -print0 | xargs -0 -I file cp -v -p file --target-directory=\$HOME
+EOF
 
 # Download Vim Plug
 echo "\n\r \e[36m--\e[32m Installing Vim-Plug \e[36m--\e[0m"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &> /dev/null
-# Install some Vim plugins
+# Install the Vim Plugins from .vimrc
 vim -c ":PlugInstall | q | q" 
-
-# Clear color
-echo "\e[0m"
-EOF
 
 # Execute and then remove the temporary script
 /bin/zsh z && rm z
+
+echo -e "\n\r \e36m--\e[32m COMPLETE \e[36m--\e[0m\n\r\n\r\e[32mExit and log back into your shell to see the changes.\e[0m"
