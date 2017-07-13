74ab65fd82a0e6dcb2fed7f9e7b66dad159106e4
Nathan Burgess <nathanburgess@users.noreply.github.com>
2017-07-13 06:00:35 -0400

    Changed ownership of resulting scripts

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # Update aptitude and install zsh
 echo -e "\n\r \e[36m--\e[32m Updating Aptitude and installing ZSH... \e[36m--\e[0m"
-sudo apt update &> /dev/null
-sudo apt install zsh -y &> /dev/null
+apt update &> /dev/null
+apt install zsh -y &> /dev/null
 
 # Set the user's default shell to ZSH
 chsh -s /bin/zsh
@@ -39,3 +39,4 @@ vim -c ":PlugInstall | q | q"
 
 echo -e "\n\r \e[36m--\e[32m COMPLETE \e[36m--\e[0m\n\r\n\r"
 source ~/.zshrc
+chown ${USER}:${USER} ~/ -R
